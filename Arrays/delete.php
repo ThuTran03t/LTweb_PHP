6b86867cd6fcc0c70716d34cb998d43fc201bed3
Ho Thi Thu Tran <hothithutran03t@gmail.com>
2023-02-21 10:08:09 +0700

    Array (group) finished

---
+++ Arrays/delete.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+require_once 'functions.php';
+
+
+if(!empty($_GET['id'])){
+     $student_id = $_GET['id'];
+        //  $student_infor = $_SESSION['student'][$student_id];
+         unset($_SESSION['student'][$student_id]);
+
+      
+}else{
+     redirect('list.php');
+     
+}
+?>
